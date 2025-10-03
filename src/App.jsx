d15f0fa5783e580f668d4d2e26ed955a9ef4f671
Taylan <taylansilva0402@gmail.com>
2025-10-03 18:48:38 -0300

    Implementing API JSON Placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,25 @@ import { useEffect, useState } from "react";
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    JSON.parse(localStorage.getItem(tasks) || []),
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        { method: "GET", }
+      );
+      const data = await response.json();
+      setTasks(data)
+    };
+    //fetchTasks();
+  }, []);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -8,8 +8,8 @@ function TaskPage() {
   const description = searchParams.get("description");
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <div className="w-[500px] space-y-6">
-        <div className="flex justify-center relative m-6">
+      <div className="w-[500px] mx-auto space-y-6">
+        <div className="flex justify-center relative mb-6">
           <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
             <ChevronLeftIcon />
           </button>
